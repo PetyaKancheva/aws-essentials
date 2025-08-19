33077c273d0d8eed989fb8aa19c8967b674b282c
PetyaKancheva <105904595+PetyaKancheva@users.noreply.github.com>
2025-08-19 13:27:08 +0300

    add snapshot test

--- json-service-app/test/json-service-app.test.ts
+++ json-service-app/test/json-service-app.test.ts
@@ -1,17 +1,11 @@
-// import * as cdk from 'aws-cdk-lib';
-// import { Template } from 'aws-cdk-lib/assertions';
-// import * as JsonServiceApp from '../lib/json-service-app-stack';
+import {App} from "aws-cdk-lib";
+import {Template} from "aws-cdk-lib/assertions";
+import {JsonServiceAppStack} from "../lib/json-service-app-stack";
 
-// example test. To run these tests, uncomment this file along with the
-// example resource in lib/json-service-app-stack.ts
-test('SQS Queue Created', () => {
-//   const app = new cdk.App();
-//     // WHEN
-//   const stack = new JsonServiceApp.JsonServiceAppStack(app, 'MyTestStack');
-//     // THEN
-//   const template = Template.fromStack(stack);
+test('Snapshot Test', () => {
+    const app = new App();
+    const stack = new  JsonServiceAppStack(app, 'MyTestStack');
+    const template = Template.fromStack(stack);
 
-//   template.hasResourceProperties('AWS::SQS::Queue', {
-//     VisibilityTimeout: 300
-//   });
+    expect(template).toMatchSnapshot();
 });
